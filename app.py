a05d1e901301156d98758bf09a4224698edc9a65
ugoMusk <ugobothankgod@gmail.com>
2023-11-25 09:29:55 +0100

    updates

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
-from app import app
+#!/usr/bin/env python3
+"""
+module defining our api endpoints
+"""
+import os
+import sys
+modelPath = os.path.abspath("./")
+sys.path.append(modelPath)
+from flask import Flask, jsonify, send_file, render_template, url_for, redirect, request
+from flask_compress import Compress
+import json
+import base64
+import chardet
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app = Flask(__name__)
+
+compress = Compress()
+compress.init_app(app)
+
+
+# @app.route("/", methods=['GET', 'POST'], strict_slashes=False)
+@app.route("/")
+def homePage():
+    """ 
+    home route
+    """
+
+    return render_template("waec-info.html")
+
+
+if __name__ == "__main__":
+    app.run()
--- app/models.py
+++
@@ -1,8 +0,0 @@
-import db_engine
-
-class Document(db_engine.Model):
-    id = db_engine.Column(db_engine.Integer, primary_key=True)
-    title = db_engine.Column(db_engine.String(100))
-    content = db_engine.Column(db_engine.Text)
-    json_data = db_engine.Column(db_engine.Text)
-n
